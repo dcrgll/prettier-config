bb15ecf61f63db88da8d09ccb0de517d7e8a2fdf
dan <8881965+dcrgll@users.noreply.github.com>
2023-03-26 22:07:27 +0100

    Update rules to match eslint config
    Update publish action

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
-  semi: false,
-  singleQuote: true,
-  trailingComma: "none",
+  arrowParens: 'always',
   printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  trailingComma: 'none',
   tabWidth: 2,
+  plugins: [require("prettier-plugin-tailwindcss")]
 }
